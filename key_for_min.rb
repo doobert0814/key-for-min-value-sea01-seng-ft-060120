119798f92efda2cbc924c143105cf01b3fa87b02
Scot Sexton <rss3509@gmail.com>
2020-05-21 17:30:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,13 @@ def key_for_min_value(name_hash)
   low_value = 0 
   low_key = NIL
   
-  name_hash.each { |key, value| 
+  name_hash.each do |key, value| 
+      if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
 
 end
